8dcf70b2254416b786c6beb52c35be77a46d0128
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2021-10-20 09:04:20 -0300

    atualizações orçamento

--- OrcamentoProject/OrcamentoPrincipal.java
+++ OrcamentoProject/OrcamentoPrincipal.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class OrcamentoPrincipal {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         ItemOrcamento[] vItem = new ItemOrcamento[100];
         ItemOrcamentoComplexo item = new ItemOrcamentoComplexo("", 0, null, 0);
         ItemOrcamento resul;
@@ -20,22 +18,22 @@ public class OrcamentoPrincipal {
                 if (pos == vItem.length) {
                     JOptionPane.showMessageDialog(null, "Vetor Cheio!", "Erro", JOptionPane.ERROR_MESSAGE);
                 } else {
-                    entrada = JOptionPane.showInputDialog(null, "Digite o histórico do item", "Histórico",
+                    entrada = JOptionPane.showInputDialog(null, "Digite o histórico do item:", "Histórico",
                             JOptionPane.INFORMATION_MESSAGE);
                     historico = entrada;
                     resul = item.encontraItem(historico);
                     while (resul != null) {
                         entrada = JOptionPane.showInputDialog(null,
-                                "Ja existe um item com esse histórico, digite um diferente", "Erro",
+                                "Já existe um item com esse histórico, digite um diferente:", "Erro",
                                 JOptionPane.ERROR_MESSAGE);
                         historico = entrada;
                         resul = item.encontraItem(historico);
                     }
-                    entrada = JOptionPane.showInputDialog(null, "Digite o valor do item", "Valor Item",
+                    entrada = JOptionPane.showInputDialog(null, "Digite o valor do item:", "Valor Item",
                             JOptionPane.INFORMATION_MESSAGE);
                     valor = Integer.parseInt(entrada);
                     while (valor <= 0) {
-                        entrada = JOptionPane.showInputDialog(null, "Valor inválido, digite um valor maior que 0",
+                        entrada = JOptionPane.showInputDialog(null, "Valor inválido, digite um valor maior que 0:",
                                 "Erro", JOptionPane.ERROR_MESSAGE);
                         valor = Integer.parseInt(entrada);
                     }
@@ -45,15 +43,16 @@ public class OrcamentoPrincipal {
                     item.setValor(valor);
                     item.setVetorItem(vItem);
                     item.setPos(pos);
-                    System.out.println("Intem registrado com sucesso!");
+                    JOptionPane.showMessageDialog(null, "Item registrado com sucesso!", "Item Cadastrado",
+                    JOptionPane.INFORMATION_MESSAGE);
                 }
                 break;
             case 2:
                 if (item.getVetorItem() == null) {
-                    JOptionPane.showMessageDialog(null, "Não exite itens registrados!", "Erro",
+                    JOptionPane.showMessageDialog(null, "Não existe itens registrados!", "Erro",
                             JOptionPane.ERROR_MESSAGE);
                 } else {
-                    entrada = JOptionPane.showInputDialog(null, "Digite o histórico do item", "Histórico",
+                    entrada = JOptionPane.showInputDialog(null, "Digite o histórico do item:", "Histórico",
                             JOptionPane.INFORMATION_MESSAGE);
                     historico = entrada;
                     resul = item.encontraItem(historico);
@@ -71,7 +70,7 @@ public class OrcamentoPrincipal {
                 break;
             case 3:
                 if (item.getVetorItem() == null) {
-                    JOptionPane.showMessageDialog(null, "Não exite itens registrados!", "Erro",
+                    JOptionPane.showMessageDialog(null, "Não existe itens registrados!", "Erro",
                             JOptionPane.ERROR_MESSAGE);
                 } else {
                     JOptionPane.showMessageDialog(null, "Total do orçamento R$" + item.getValor(), "Total Valor",
@@ -84,9 +83,8 @@ public class OrcamentoPrincipal {
 
                 break;
             default:
-                JOptionPane.showMessageDialog(null, "Opção inválida, digite 1, 2, 3 ou 0 para encerrar!", "Erro",
+                JOptionPane.showMessageDialog(null, "Opção inválida, digite 1, 2, 3 ou 0 para encerrar:", "Erro",
                         JOptionPane.ERROR_MESSAGE);
-                System.err.println("Opcao invalida, digite 1, 2, 3 ou 0 para encerrar!");
             }
 
         } while (op != 0);
@@ -94,7 +92,7 @@ public class OrcamentoPrincipal {
 
     public static String menu() {
         String menu;
-        menu = "1 - Adicionar item\n2 - Buscar item\n3 - Valor de todos os items\n0 - Encerrar";
+        menu = "1 - Adicionar item\n2 - Buscar item\n3 - Valor de todos os itens\n0 - Encerrar";
         return menu;
     }
 }
