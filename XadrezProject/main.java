3a06b0b1dda5dea6c0b2f2373ed4617384f8d196
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2021-10-26 18:41:52 -0300

    adição do joption no xadrez

--- XadrezProject/main.java
+++ XadrezProject/main.java
@@ -1,11 +1,9 @@
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
 public class main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+
         String entrada;
         int linhaOrigem, colunaOrigem, op, linhaDestino, colunaDestino, jogada;
         String[][] tabuleiro = { { "R", "N", "B", "Q", "K", "B", "N", "R" }, { "P", "P", "P", "P", "P", "P", "P", "P" },
@@ -20,7 +18,7 @@ public class main {
             linhaOrigem = Integer.parseInt(entrada);
             while (linhaOrigem < 1 || linhaOrigem > 8) { // validacao
                 entrada = JOptionPane.showInputDialog(null,
-                        imprimirTabuleiro(tabuleiro) + "\nOpcao invalida. Digite um numero entre 1 e 8 ", "ERRO",
+                        imprimirTabuleiro(tabuleiro) + "\nOpção invalida. Digite um numero entre 1 e 8: ", "ERRO",
                         JOptionPane.ERROR_MESSAGE);
                 linhaOrigem = Integer.parseInt(entrada);
             }
@@ -30,7 +28,6 @@ public class main {
                     imprimirTabuleiro(tabuleiro) + "\nInfome a posição da coluna do tabuleiro a ser selecionada: ",
                     "TABULEIRO", JOptionPane.QUESTION_MESSAGE);
             colunaOrigem = Integer.parseInt(entrada);
-            colunaOrigem = in.nextInt();
             while (colunaOrigem < 1 || colunaOrigem > 8) { // validacao
                 entrada = JOptionPane.showInputDialog(null,
                         imprimirTabuleiro(tabuleiro) + "\nInfome a posição da coluna do tabuleiro a ser selecionada: ",
@@ -41,21 +38,30 @@ public class main {
             if (jogada == 0) {
                 // jogada das brancas
                 if (tabuleiro[linhaOrigem][colunaOrigem] != "p") {
-                    System.out.println("Opa posicao incorreta");
+                    JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                            JOptionPane.ERROR_MESSAGE);
                 } else {
-                    System.out.print("Infome a posição da linha do tabuleiro para se mover: ");
-                    linhaDestino = in.nextInt();
+                    entrada = JOptionPane.showInputDialog(null,
+                            imprimirTabuleiro(tabuleiro) + "\nInfome a posição da linha do tabuleiro para se mover: ",
+                            "TABULEIRO", JOptionPane.QUESTION_MESSAGE);
+                    linhaDestino = Integer.parseInt(entrada);
                     while (linhaDestino < 1 || linhaDestino > 8) { // validacao
-                        System.out.print("Opção inválida. Digite um numero entre 1 e 8: ");
-                        linhaDestino = in.nextInt();
+                        entrada = JOptionPane.showInputDialog(null,
+                                imprimirTabuleiro(tabuleiro) + "\nOpção inválida. Digite um numero entre 1 e 8: ",
+                                "ERRO", JOptionPane.ERROR_MESSAGE);
+                        linhaDestino = Integer.parseInt(entrada);
                     }
                     linhaDestino -= 1; // ajustar posicao da matriz
 
-                    System.out.print("Infome a posição da coluna do tabuleiro para se mover: ");
-                    colunaDestino = in.nextInt();
+                    entrada = JOptionPane.showInputDialog(null,
+                            imprimirTabuleiro(tabuleiro) + "\nInfome a posição da coluna do tabuleiro para se mover: ",
+                            "COLUNA", JOptionPane.QUESTION_MESSAGE);
+                    colunaDestino = Integer.parseInt(entrada);
                     while (colunaDestino < 1 || colunaDestino > 8) { // validacao
-                        System.out.print("Opção inválida. Digite um numero entre 1 e 8: ");
-                        colunaDestino = in.nextInt();
+                        entrada = JOptionPane.showInputDialog(null,
+                                imprimirTabuleiro(tabuleiro) + "\nOpção inválida. Digite um numero entre 1 e 8: ",
+                                "ERRO", JOptionPane.QUESTION_MESSAGE);
+                        colunaDestino = Integer.parseInt(entrada);
                     }
                     colunaDestino -= 1; // ajustar posicao da matriz
 
@@ -67,44 +73,58 @@ public class main {
                                 tabuleiro[linhaDestino][colunaDestino] = tabuleiro[linhaOrigem][colunaOrigem];
                                 tabuleiro[linhaOrigem][colunaOrigem] = " ";
                                 jogada = 1;
-                                System.out.println("Peça movida");
+                                JOptionPane.showMessageDialog(null, "Peça movida", "SUCESSO",
+                                        JOptionPane.INFORMATION_MESSAGE);
                             } else {
-                                System.out.println("Essa posição nao pode ser selecionada");
+                                JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                                        JOptionPane.ERROR_MESSAGE);
                             }
                         } else {
                             if (linhaOrigem - 1 == linhaDestino && tabuleiro[linhaDestino][colunaDestino] == " ") {
                                 tabuleiro[linhaDestino][colunaDestino] = tabuleiro[linhaOrigem][colunaOrigem];
                                 tabuleiro[linhaOrigem][colunaOrigem] = " ";
                                 jogada = 1;
-                                System.out.println("Peça movida");
+                                JOptionPane.showMessageDialog(null, "Peça movida", "SUCESSO",
+                                        JOptionPane.INFORMATION_MESSAGE);
                             } else {
-                                System.out.println("Essa posição nao pode ser selecionada");
+                                JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                                        JOptionPane.ERROR_MESSAGE);
                             }
                         }
 
                     } else {
-                        System.out.println("Essa posição não pode ser selecionada");
+                        JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                                JOptionPane.ERROR_MESSAGE);
                     }
                 }
 
             } else {
                 // jogada das pretas.
                 if (tabuleiro[linhaOrigem][colunaOrigem] != "P") {
-                    System.out.println("Opa posição incorreta");
+                    JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                            JOptionPane.ERROR_MESSAGE);
                 } else {
-                    System.out.print("Infome a posição da linha do tabuleiro para se mover: ");
-                    linhaDestino = in.nextInt();
+                    entrada = JOptionPane.showInputDialog(null,
+                            imprimirTabuleiro(tabuleiro) + "\nInfome a posição da linha do tabuleiro para se mover: ",
+                            "TABULEIRO", JOptionPane.QUESTION_MESSAGE);
+                    linhaDestino = Integer.parseInt(entrada);
                     while (linhaDestino < 1 || linhaDestino > 8) { // validacao
-                        System.out.print("Opção inválida. Digite um numero entre 1 e 8: ");
-                        linhaDestino = in.nextInt();
+                        entrada = JOptionPane.showInputDialog(null,
+                                imprimirTabuleiro(tabuleiro) + "\nOpção inválida. Digite um numero entre 1 e 8: ",
+                                "ERRO", JOptionPane.ERROR_MESSAGE);
+                        linhaDestino = Integer.parseInt(entrada);
                     }
                     linhaDestino -= 1; // ajustar posicao da matriz
 
-                    System.out.print("Infome a posição da coluna do tabuleiro para se mover: ");
-                    colunaDestino = in.nextInt();
+                    entrada = JOptionPane.showInputDialog(null,
+                            imprimirTabuleiro(tabuleiro) + "\nInfome a posição da coluna do tabuleiro para se mover: ",
+                            "COLUNA", JOptionPane.QUESTION_MESSAGE);
+                    colunaDestino = Integer.parseInt(entrada);
                     while (colunaDestino < 1 || colunaDestino > 8) { // validacao
-                        System.out.print("Opção inválida. Digite um numero entre 1 e 8: ");
-                        colunaDestino = in.nextInt();
+                        entrada = JOptionPane.showInputDialog(null,
+                                imprimirTabuleiro(tabuleiro) + "\nOpção inválida. Digite um numero entre 1 e 8: ",
+                                "ERRO", JOptionPane.ERROR_MESSAGE);
+                        colunaDestino = Integer.parseInt(entrada);
                     }
                     colunaDestino -= 1; // ajustar posicao da matriz
 
@@ -115,35 +135,42 @@ public class main {
                                 tabuleiro[linhaDestino][colunaDestino] = tabuleiro[linhaOrigem][colunaOrigem];
                                 tabuleiro[linhaOrigem][colunaOrigem] = " ";
                                 jogada = 0;
-                                System.out.println("Peça movida");
+                                JOptionPane.showMessageDialog(null, "Peça movida", "SUCESSO",
+                                        JOptionPane.INFORMATION_MESSAGE);
                             } else {
-                                System.out.println("Essa posição não pode ser selecionada!");
+                                JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                                        JOptionPane.ERROR_MESSAGE);
                             }
                         } else {
                             if (linhaOrigem + 1 == linhaDestino && tabuleiro[linhaDestino][colunaDestino] == " ") {
                                 tabuleiro[linhaDestino][colunaDestino] = tabuleiro[linhaOrigem][colunaOrigem];
                                 tabuleiro[linhaOrigem][colunaOrigem] = " ";
                                 jogada = 0;
-                                System.out.println("Peça movida");
+                                JOptionPane.showMessageDialog(null, "Peça movida", "SUCESSO",
+                                        JOptionPane.INFORMATION_MESSAGE);
                             } else {
-                                System.out.println("Essa posição não pode ser selecionada!");
+                                JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                                        JOptionPane.ERROR_MESSAGE);
                             }
                         }
                     } else {
-                        System.out.println("Essa posição não pode ser selecionada!");
+                        JOptionPane.showMessageDialog(null, "Essa posição não pode ser selecionada", "ERRO",
+                                JOptionPane.ERROR_MESSAGE);
                     }
                 }
 
             }
-            System.out.println(" Quer continuar o jogo? 1 - SIM, 0 - NÃO");
-            op = in.nextInt();
+            entrada = JOptionPane.showInputDialog(null, "Quer continuar o jogo? 1 - SIM, 0 - NÃO: ", "JOGO",
+                    JOptionPane.QUESTION_MESSAGE);
+            op = Integer.parseInt(entrada);
+            while (op != 0 && op != 1) {
+                entrada = JOptionPane.showInputDialog(null, "Opção inválida. Digite um numero 0 ou 1: ", "ERRO",
+                        JOptionPane.ERROR_MESSAGE);
+                op = Integer.parseInt(entrada);
+            }
         } while (op != 0);
 
         imprimirTabuleiro(tabuleiro);
-    }
-
-    public static void jogar(String[][] t, int li, int co) {
-
     }
 
     public static String imprimirTabuleiro(String[][] t) {
