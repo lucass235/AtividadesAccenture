bafa946281dc0e35393b313f19461753db58a15b
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2021-10-28 18:23:04 -0300

    Update Peao.java

--- XadrezProject/Peao.java
+++ XadrezProject/Peao.java
@@ -4,7 +4,7 @@ public class Peao extends Peca {
     public String[][] mover (String t[][], String peca, int linha, int coluna) {
         t[linha][coluna] = peca;
         super.setTabuleiro(t);
-    
+  
         return super.getTabuleiro();
    }
 }
