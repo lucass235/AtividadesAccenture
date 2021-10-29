1a95a3a013d2fdfd75d9caeb487d5d2889bd0dd8
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2021-10-28 23:01:36 -0300

    n

--- StramProject/src/br/edu/presentation/StreamsLucasSantos.java
+++ StramProject/src/br/edu/presentation/StreamsLucasSantos.java
@@ -3,6 +3,7 @@ package br.edu.presentation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -19,7 +20,7 @@ import br.edu.service.ServiceFactory;
  * else e switch-case
  * 
  */
-public class Principal {
+public class StreamsLucasSantos {
 
 	private static BankService service = ServiceFactory.getService();
 
@@ -28,6 +29,8 @@ public class Principal {
 	private static List<Account> contas = new ArrayList<>();
 	private static List<String> estado = new ArrayList<>();
 	private static List<String> emails = new ArrayList<>();
+	private static List<Integer> numeroContaPais = new ArrayList<>();
+	private static List<Account> contasJoin = new ArrayList<>();
 
 	public static void main(String[] args) {
 		// main com os clientes, enderecos e contas.
@@ -52,7 +55,7 @@ public class Principal {
 		Account conta1 = new Account(011, 001, 100.0, cliente1, AccountEnum.CHECKING);
 		Account conta2 = new Account(011, 002, 100.0, cliente2, AccountEnum.CHECKING);
 		Account conta3 = new Account(022, 003, 200.0, cliente3, AccountEnum.SAVING);
-		Account conta4 = new Account(022, 004, 200.0, cliente4, AccountEnum.SAVING);
+		Account conta4 = new Account(022, 004, 200.0, cliente4, AccountEnum.JOINT);
 		contas.add(conta1);
 		contas.add(conta2);
 		contas.add(conta3);
@@ -80,10 +83,18 @@ public class Principal {
 			System.out.println("Estados Unidos possui clientes mais ricos!");
 		}
 
-		System.out.println();
+		System.out.println("====================================");
 
 		// * 4. Imprime na tela o saldo médio das contas da agência
-		imprimirSaldoMedio(011);
+		imprimirSaldoMedio(conta2.getAgency());
+
+		System.out.println("====================================");
+
+		/**
+		 * 5. Imprime na tela o nome de todos os clientes que possuem conta poupança
+		 * (tipo SAVING)
+		 */
+		imprimirClientesComPoupanca();
 
 		System.out.println();
 
@@ -93,73 +104,92 @@ public class Principal {
 		estado = getEstadoClientes(011);
 		System.out.println(estado);
 
-		System.out.println();
+		System.out.println("====================================");
+
+		// * 7.
+		// * @param country
+		// * @return Retorna uma lista de inteiros com os números das contas daquele
+		// país
+		numeroContaPais = getNumerosContas("United States");
+		System.out.println("Numero de contas dos Estados Unidos " + numeroContaPais);
+
+		System.out.println("====================================");
 
 		// * 8. Retorna o somatório dos saldos das contas do cliente em questão
 		// * @param clientEmail
 		// * @return
 		System.out.println(getMaiorSaldo(cliente2.getEmail()));
 
-		System.out.println();
+		System.out.println("====================================");
 
-		//  * 9. Realiza uma operação de saque na conta de acordo com os parâmetros
-		//  * recebidos
-		//  * @param agency
-		//  * @param number
-		//  * @param value
+		// * 9. Realiza uma operação de saque na conta de acordo com os parâmetros
+		// * recebidos
+		// * @param agency
+		// * @param number
+		// * @param value
 		sacar(011, 002, 50.0);
 		System.out.println("Saque realizado");
 
-		System.out.println();
+		System.out.println("====================================");
 
-	//  * 10. Realiza um deposito para todos os clientes do país em questão
-	//  * @param country
-	//  * @param value
-	depositar("United States", 20.0);
-	System.out.println("Valor depositado na conta do pais US");
+		// * 10. Realiza um deposito para todos os clientes do país em questão
+		// * @param country
+		// * @param value
+		depositar("United States", 20.0);
+		System.out.println("Valor depositado na conta do pais US");
 
-	System.out.println();
+		System.out.println("====================================");
 
-	//  * 11. Realiza uma transferência entre duas contas de uma agência.
-	//  * @param agency       - agência das duas contas
-	//  * @param numberSource - conta a ser debitado o dinheiro
-	//  * @param numberTarget - conta a ser creditado o dinheiro
-	//  * @param value        - valor da transferência
-	transferir(001, 002, 002, 50.0);
-	System.out.println("Valor trasferido");
+		// * 11. Realiza uma transferência entre duas contas de uma agência.
+		// * @param agency - agência das duas contas
+		// * @param numberSource - conta a ser debitado o dinheiro
+		// * @param numberTarget - conta a ser creditado o dinheiro
+		// * @param value - valor da transferência
+		transferir(001, 002, 002, 50.0);
+		System.out.println("Valor trasferido");
 
-	System.out.println();
+		System.out.println("====================================");
 
-	//  * 13.
-	//  * @param state
-	//  * @return Retorna uma lista com o somatório dos saldos de todas as contas do
-	//  *         estado
-	double saldoEstados = getSomaContasEstado("PE");
-	System.out.println("Saldos do estado PE:" + saldoEstados);
+		// * 12.
+		// * @param clients
+		// * @return Retorna uma lista com todas as contas conjuntas (JOINT) dos
+		// clientes
+		// */ // List<Client> clients
+		contasJoin = getContasConjuntas();
+		System.out.println("Contas com join: " + contasJoin);
 
-	System.out.println();
+		System.out.println("====================================");
 
-	//  * 14.
-	//  * @return Retorna um array com os e-mails de todos os clientes que possuem
-	//  *         contas conjuntas
+		// * 13.
+		// * @param state
+		// * @return Retorna uma lista com o somatório dos saldos de todas as contas do
+		// * estado
+		double saldoEstados = getSomaContasEstado("PE");
+		System.out.println("Saldos do estado PE:" + saldoEstados);
+
+		System.out.println("====================================");
+
+		// * 14.
+		// * @return Retorna um array com os e-mails de todos os clientes que possuem
+		// * contas conjuntas
 		emails = getEmailsClientesContasConjuntas();
 		System.out.println("Emails contas conjuntas: " + emails);
 
+		System.out.println("====================================");
+
+		// * 15.
+		// * @param number
+		// * @return Retorna se o número é primo ou não
+		int num = 5;
+		boolean ePrimo = isPrimo(num);
+		System.out.println("O numero " + num + " e primo? --> " + ePrimo);
+
 		System.out.println();
 
-	//  * 15.
-	//  * @param number
-	//  * @return Retorna se o número é primo ou não
-	int num = 5;
-	boolean ePrimo = isPrimo(num);
-	System.out.println("O numero "+num+" e primo? --> "+ePrimo);
-
-	System.out.println();
-
-	//  * 16.
-	//  * @param number
-	//  * @return Retorna o fatorial do número
-	System.out.println("O fatorial de "+num+" e:"+ getFatorial(num));
+		// * 16.
+		// * @param number
+		// * @return Retorna o fatorial do número
+		System.out.println("O fatorial de " + num + " e:" + getFatorial(num));
 
 	}
 
@@ -195,12 +225,9 @@ public class Principal {
 		System.out.println(average);
 	}
 
-	/**
-	 * 5. Imprime na tela o nome de todos os clientes que possuem conta poupança
-	 * (tipo SAVING)
-	 */
 	public static void imprimirClientesComPoupanca() {
-		// Complete o código aqui
+		contas.stream().filter(conta -> conta.getType().equals(AccountEnum.SAVING))
+				.map(conta -> conta.getClient().getName()).distinct().forEach(System.out::println);
 
 	}
 
@@ -210,13 +237,11 @@ public class Principal {
 		return (List<String>) stateOfAllClients;
 	}
 
-	// * 7.
-	// * @param country
-	// * @return Retorna uma lista de inteiros com os números das contas daquele
-	// país
-
 	public static List<Integer> getNumerosContas(String country) {
 		// Complete o código aqui
+		List<Integer> countryNumbers = contas.stream()
+				.filter(conta -> conta.getClient().getAddress().getCountry() == country).map(conta -> conta.getNumber())
+				.collect(Collectors.toList());
 
 		return countryNumbers;
 	}
@@ -262,15 +287,11 @@ public class Principal {
 
 	}
 
-	/**
-	 * 12.
-	 * 
-	 * @param clients
-	 * @return Retorna uma lista com todas as contas conjuntas (JOINT) dos clientes
-	 */ // List<Client> clients
 	public static List<Account> getContasConjuntas() {
 		List<Account> jointAccounts = new ArrayList<Account>();
 		// Complete o código aqui
+		jointAccounts = contas.stream().filter(conta -> conta.getType().equals(AccountEnum.JOINT)).map(conta -> conta)
+				.collect(Collectors.toList());
 
 		return jointAccounts;
 	}
@@ -293,7 +314,7 @@ public class Principal {
 	public static boolean isPrimo(int number) {
 		return IntStream.rangeClosed(2, (number / 2)).noneMatch(i -> number % i == 0);
 	}
-	
+
 	public static int getFatorial(int number) {
 		int factorial = IntStream.rangeClosed(1, number).reduce(1, (x, y) -> x * y);
 		return factorial;
