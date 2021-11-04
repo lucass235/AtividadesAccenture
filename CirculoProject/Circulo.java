eecb935905cba2e27c1b04433285176e93ebf2e1
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2021-11-04 08:43:30 -0300

    o

--- CirculoProject/Circulo.java
+++ CirculoProject/Circulo.java
@@ -6,30 +6,26 @@ public class Circulo {
     private float perimetro;
 
     public Circulo(double raio) {
-
-    this.raio = (float) raio;
+        this.raio = (float) raio;
     }
 
     public float getRaio() {
         return raio;
     }
 
-
     public void setRaio(double newRaio) {
-        
         this.raio = (float) newRaio;
     }
 
     public void calcularArea() {
-
         this.area = (Math.pow(this.raio, 2) * Math.PI);
-        JOptionPane.showMessageDialog(null, "Raio: "+this.raio+"\nResultado: "+this.area, "Area", JOptionPane.INFORMATION_MESSAGE);
-        
+        JOptionPane.showMessageDialog(null, "Raio: " + this.raio + "\nResultado: " + this.area, "Area",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
     public void calcularPerimetro() {
-
         this.perimetro = (2 * Math.PI * this.raio);
-        JOptionPane.showMessageDialog(null, "Raio: "+this.raio+"\nResultado: "+this.perimetro, "Perimetro", JOptionPane.INFORMATION_MESSAGE); 
-    } 
+        JOptionPane.showMessageDialog(null, "Raio: " + this.raio + "\nResultado: " + this.perimetro, "Perimetro",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
 }
