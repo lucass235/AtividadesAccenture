ca218c9966fbc443762f518b173424a41eae07c2
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2021-11-04 08:41:17 -0300

    novo

--- CirculoProject/Circulo.java
+++ CirculoProject/Circulo.java
@@ -1,32 +1,35 @@
 import javax.swing.JOptionPane;
 
 public class Circulo {
-    private Float raio;
-    private Float area;
-    private Float perimetro;
+    private float raio;
+    private float area;
+    private float perimetro;
 
     public Circulo(double raio) {
 
-    this.raio = new Float(raio);
+    this.raio = (float) raio;
+    }
 
+    public float getRaio() {
+        return raio;
     }
 
 
     public void setRaio(double newRaio) {
         
-        this.raio = new Float(newRaio);
+        this.raio = (float) newRaio;
     }
 
     public void calcularArea() {
 
-        this.area = new Float(Math.pow(this.raio, 2) * Math.PI);
+        this.area = (Math.pow(this.raio, 2) * Math.PI);
         JOptionPane.showMessageDialog(null, "Raio: "+this.raio+"\nResultado: "+this.area, "Area", JOptionPane.INFORMATION_MESSAGE);
         
     }
 
     public void calcularPerimetro() {
 
-        this.perimetro = new Float(2 * Math.PI * this.raio);
+        this.perimetro = (2 * Math.PI * this.raio);
         JOptionPane.showMessageDialog(null, "Raio: "+this.raio+"\nResultado: "+this.perimetro, "Perimetro", JOptionPane.INFORMATION_MESSAGE); 
     } 
 }
