e5d75c7f7437d45445e6e0583bb4bfd8b6780f41
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2021-11-04 10:05:20 -0300

    novo

---
+++ JogoProject/Jogo.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+
+public class Jogo {
+    private Time mandante;
+    private Time visitante;
+    private Date dataDoJogo;
+    private String estadio;
+    private int placarMandante;
+    private int placarVisitante;
+
+    public Jogo() {
+    }
+
+    public Jogo(Time mandante, Time visitante, Date dataDoJogo, String estadio, int placarMandante, int placarVisitante) {
+        this.mandante = mandante;
+        this.visitante = visitante;
+        this.dataDoJogo = dataDoJogo;
+        this.estadio = estadio;
+        this.placarMandante = placarMandante;
+        this.placarVisitante = placarVisitante;
+    }
+
+    public void gerarResultado() {
+        int golsMandante = (int) (Math.random() * 5);
+        int golsVisitante = (int) (Math.random() * 5);
+        this.placarMandante = golsMandante;
+        this.placarVisitante = golsVisitante;
+    }
+
+    public void gerarLesoes () {
+        int lesoesMandante = (int) (Math.random() * 5);
+        int lesoesVisitante = (int) (Math.random() * 5);
+        this.mandante.setLesoes(lesoesMandante);
+        this.visitante.setLesoes(lesoesVisitante);
+    }
+
+    public void gerarCartoes () {
+        int cartoesMandante = (int) (Math.random() * 5);
+        int cartoesVisitante = (int) (Math.random() * 5);
+        this.mandante.setCartoes(cartoesMandante);
+        this.visitante.setCartoes(cartoesVisitante);
+    }
+
+    public void permitirTreinamento () {
+        this.mandante.treinar();
+        this.visitante.treinar();
+    }
+}
---
+++ JogoProject/MainJogo.java
@@ -0,0 +1,5 @@
+public class MainJogo {
+    public static void main(String[] args) {
+        System.out.println("classe jogo");
+    }
+}
